191f80eb21a310b11eaaa94ba3fe228e1044f49e
tejava <dlwnsgml94@snu.ac.kr>
2019-09-11 01:01:21 +0900

    implemented constructor and method to check the form

--- hw1/javascript/form.js
+++ hw1/javascript/form.js
@@ -9,8 +9,40 @@ class Form {
       age,
       birth_month,
       birth_day,
-      birth_year) {}
+      birth_year) {
+        this.email = email;
+        this.password = password;
+        this.password_confirmation = password_confirmation;
+        this.phone_number = phone_number;
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.birth_month = birth_month;
+        this.birth_day = birth_day;
+        this.birth_year = birth_year;
+      }
     // TODO: You may fill in functions in the class.
+
+    // function to check the form and return adequate message for popup
+    checkForm() {
+      // set empty string to make it easy to return + boolean to check whether it's successful
+      var isSuccess = true;
+      var message = "";
+
+      var patt = new RegExp("@")
+      if (!patt.test(this.email)) {
+        isSuccess = false;
+        message += "Email\n"
+      }
+
+      // return it with appropriate message
+      if (isSuccess) {
+        return "Successfully Submitted!";
+      }
+      else {
+        return "You must correct:\n" + message;
+      }
+    }
 
   }
 
@@ -19,11 +51,31 @@ class Form {
   but.onclick = function() {
       var email = document.forms["form"]["email"].value;
       // TODO: Fill in the rest of the function. Use the Form class defined above
+      var password = document.forms["form"]["password"].value;
+      var password_confirmation = document.forms["form"]["password-confirmation"].value;
+      var phone_number = document.forms["form"]["phone-number"].value;
+      var fname = document.forms["form"]["fname"].value;
+      var lname = document.forms["form"]["lname"].value;
+      var age = document.forms["form"]["age"].value;
+      var birth_month = document.forms["form"]["birth-month"].value;
+      var birth_day = document.forms["form"]["birth-day"].value;
+      var birth_year = document.forms["form"]["birth-year"].value;
 
-      var form;
+      var form = new Form(
+        email,
+        password,
+        password_confirmation,
+        phone_number,
+        fname,
+        lname,
+        age,
+        birth_month,
+        birth_day,
+        birth_year);
 
       let alertMessage = '';
       // TODO: Fill the alert message according to the validation result by following the form in README.md.
+      alertMessage = form.checkForm();
       alert(alertMessage);
 
       // Hint: you can use the RegExp class for matching a string with the `test` method.
